dad88039c1a3fb26dc635cda74a93c29e65c4f94
Matheus <150362833+yMatHezZ@users.noreply.github.com>
2024-06-24 12:08:16 -0300

    php

---
+++ entrar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro/Login</title>
+    <link rel="stylesheet" type="text/css" href="entrar.css" media="screen" />
+</head>
+<body> <div class="logo"></div>
+    <div class="titulo">BEM VINDO A MC</div>
+    <div class="container">
+        <a href="pg_cadastro.html" class="btn btn-cadastro">Cadastro</a>
+        <a href="Pg_login.html" class="btn btn-login">Login</a>
+    </div>
+</body>
+</html>
