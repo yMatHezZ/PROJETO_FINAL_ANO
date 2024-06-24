88069b89523fd5eb3605d12ade0944f4424291a0
Matheus <150362833+yMatHezZ@users.noreply.github.com>
2024-06-24 12:08:32 -0300

    php

---
+++ jeandtl.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalhes do Produto</title>
+    <link rel="stylesheet" type="text/css" href="detal_prod.css">
+</head>
+<body> 
+    <header>
+        <div class="logo"></div>
+    </header>
+    <div class="container">
+        <div class="product-details">
+            <div class="product-image">
+                <img src="calcajeans1.jpg" alt="Jeans">
+            </div>
+            <div class="product-info">
+                <h2>Relógio IWC</h2>
+                <p class="price">R$14000.00</p>
+                <p class="description">Movimento do cronógrafo mecânico, corda automática, calibre manufaturado IWC 69380, reserva de marcha de 46 horas, indicação da data e dos dias da semana, caixa interior em ferro macio para o isolamento contra campos magnéticos, coroa de rosca, vidro de safira, bombeado, antirreflexo de ambos os lados, gravura no fundo com o logotipo TOP GUN, resistente à água 6 bar, altura da caixa 15,7mm e diâmetro 44,5mm.
+                </p>
+                  <br>
+                <button class="add-to-cart" href="carrinho.html">Adicionar ao Carrinho</button>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; MC</p>
+    </footer>
+</body>
+</html>
