0d470a425a25d944b68caf1e3de83ae00626778e
Matheus <150362833+yMatHezZ@users.noreply.github.com>
2024-06-24 12:09:12 -0300

    php

---
+++ moletommerc.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalhes do Produto</title>
+    <link rel="stylesheet" type="text/css" href="detal_prod.css">
+</head>
+<body> 
+    <header>
+        <div class="logo"></div>
+    </header>
+    <div class="container">
+        <div class="product-details">
+            <div class="product-image">
+                <img src="moletommercedes.jpg" alt="moletom">
+            </div>
+            <div class="product-info">
+                <h2>Corrente de cruz</h2>
+                <p class="price">R$240.00</p>
+                <p class="description">Eleve seu estilo com nossa corrente de prata, vai bem em mixes de colares e combina com joias em prata, ouro e outros materiais.
+
+                </p>
+                <button class="add-to-cart" href="carrinho.html">Adicionar ao Carrinho</button>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; MC</p>
+    </footer>
+</body>
+</html>
