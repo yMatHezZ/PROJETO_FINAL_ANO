7df655a3dc296e880d1d651f1668b558a2166f70
Matheus <150362833+yMatHezZ@users.noreply.github.com>
2024-06-24 12:08:58 -0300

    php

---
+++ maiodet.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalhes do Produto</title>
+    <link rel="stylesheet" type="text/css" href="detl_prod_fem.css">
+</head>
+<body> 
+    <header>
+        <div class="logo"></div>
+    </header>
+    <div class="container">
+        <div class="product-details">
+            <div class="product-image">
+                <img src="maio.jpeg" alt="maio">
+            </div>
+            <div class="product-info">
+                <h2>Pulseira</h2>
+                <p class="price">R$180.00</p>
+                <p class="description">Esta é uma descrição breve da pulseira, O minimalismo das pulseiras lisas representa um estilo impecável para qualquer ocasião.</p>
+                
+                
+                <button class="add-to-cart" href="carrinho.html">Adicionar ao Carrinho</button>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; MC</p>
+    </footer>
+</body>
+</html>
