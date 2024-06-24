8ccc2e3d226b1620b6fdac92520ef0cd70d6b04f
Matheus <150362833+yMatHezZ@users.noreply.github.com>
2024-06-24 12:08:24 -0300

    php

---
+++ jaqueta.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalhes do Produto</title>
+    <link rel="stylesheet" type="text/css" href="detal_prod.css">
+</head>
+<body> 
+    <header>
+        <div class="logo"></div>
+    </header>
+    <div class="container">
+        <div class="product-details">
+            <div class="product-image">
+                <img src="jaquetadecouro.jpg" alt="moletom">
+            </div>
+            <div class="product-info">
+                <h2>Jaqueta de couro</h2>
+                <p class="price">R$850.00</p>
+                <p class="description">Conquiste as ruas com a Jaqueta Masculina de Couro com Capuz. Design moderno e proteção contra o frio em um só produto!
+                </p> 
+                <h3>Escolha o tamanho:</h3>
+                <div class="tamanho-selector">
+                    <select id="tamanho">
+                      <option value="PP">PP</option>
+                      <option value="P">P</option>
+                      <option value="M">M</option>
+                      <option value="G">G</option>
+                      <option value="GG">GG</option>
+                    </select>
+                  </div>
+                  <br>
+                <button class="add-to-cart" href="carrinho.html">Adicionar ao Carrinho</button>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; MC</p>
+    </footer>
+</body>
+</html>
