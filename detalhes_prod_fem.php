66d3f2c7386d8263ccf57bca431d7a2328833696
Matheus <150362833+yMatHezZ@users.noreply.github.com>
2024-06-24 12:07:14 -0300

    php

---
+++ detalhes_prod_fem.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalhes do Produto</title>
+    <link rel="stylesheet" type="text/css" href="detl_prod_fem.css">
+</head>
+<body> 
+    <header>
+        <div class="logo"></div>
+    </header>
+    <section>
+    <div class="container">
+        <div class="product-details">
+            <div class="product-image">
+                <img src="cropped.jpeg" alt="cropped">
+            </div>
+            <div class="product-info">
+                <h2>Cropped</h2>
+                <p class="price">R$100.00</p>
+                <p class="description">Esta é uma descrição breve do cropped. Camiseta curta confeccionada em mistura de algodão que traz conforto e versatilidade.</p>
+                <div class="size-options">
+                    <label for="size">Selecione o tamanho:</label>
+                    <select id="size">
+                        <option value="pp">PP</option>
+                        <option value="p">P</option>
+                        <option value="m">M</option>
+                        <option value="g">G</option>
+                        <option value="gg">GG</option>
+                    </select>
+                </div>
+                <button class="add-to-cart" href="carrinho.html">Adicionar ao Carrinho</button>
+            </div>
+        </div>
+    </div>
+</section>
+    <footer>
+        <p>&copy; MC</p>
+    </footer>
+</body>
+</html>
